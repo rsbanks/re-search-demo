3131540b9013ce204bed8eab819dbc63f2b7ae59
blhuynh1999 <57556053+blhuynh1999@users.noreply.github.com>
2020-05-22 21:03:00 -0500

    Removed call to getProfs

--- runserver.py
+++ runserver.py
@@ -706,11 +706,9 @@ def submitPreferences():
     # insert data into 'preferences database'
     profPrefDB = profPreferencesDB()
     error_statement = profPrefDB.connect()
-    # added for demo version
-    profs, error_statement2 = getProfs('netid ILIKE %s',[""])
     if error_statement == '' :
     	# generate random username for demo
-        report = profPrefDB.createProfPreference(["username"+random.randint(0,5*profs.len()), courseSelection,
+        report = profPrefDB.createProfPreference(["username"+random.randint(0,100), courseSelection,
             advisor1, advisor1Comments, advisor2, advisor2Comments, advisor3, 
             advisor3Comments, advisor4, advisor4Comments, submittedTime, completedTime])
         profPrefDB.disconnect()
